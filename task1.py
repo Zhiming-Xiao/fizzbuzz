44593d4e0dcab1e91f7e993006000b73a66d6c96
Zhiming-Xiao <zh8935942dal.ca>
2020-02-20 10:26:44 -0800

    finish 3

--- task1.py
+++ task1.py
@@ -24,4 +24,23 @@ def sub_task2():
 			d[iy].append(int(r["OBJECTID"]))
 	print(d)
 	
-sub_task2()
+#sub_task2()
+
+def sub_task3():
+	df = pd.read_csv("Trails.csv",usecols=["OBJECTID","LIGHT","STATUS","WALKING","BIKING"])
+	count_light = 0
+	count_act = 0
+	count_wb = 0
+	for i,r in df.iterrows():
+		if r["LIGHT"] == "Yes":
+			count_light += 1
+		if r["STATUS"] == "ACTIVE":
+			count_act += 1
+		if r["WALKING"] == "Yes" and r["BIKING"] == "Yes":
+			count_wb += 1
+	
+	print(f"There are {count_light} have lighting")
+	print(f"There are {count_act} currently Active")
+	print(f'There are {count_wb} both "Walking" and have "Biking"')
+	
+sub_task3()
