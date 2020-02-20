f0dc94b102fb256c062d8f42e48bfbc5a224d58e
Zhiming-Xiao <zh8935942dal.ca>
2020-02-20 11:38:55 -0800

    3

--- task3.py
+++ task3.py
@@ -16,7 +16,7 @@ def count_the_article(b):
 	with open(b,"r") as f:
 		ft = f.read()
 		
-	nt = ft.split()
+	nt = re.findall("[a-zA-Z-]+",ft)
 	
 	l = ["a", "the", "at", "run", "to","and","are","or","for","an","this"]
 	c = 0
@@ -26,7 +26,22 @@ def count_the_article(b):
 	print(c)
 	return c
 	
-count_the_article("Book1.txt")
+#count_the_article("Book1.txt")
 
+def sorted_words(b):
+	with open(b,"r") as f:
+		ft = f.read()
+		
+	nt = re.findall("[a-zA-Z-]+",ft)
 	
-	
+	d = {}
+	for i in nt:
+		d[i] = d.get(i,0) + 1
+	l=[]	
+	for key, value in sorted(d.items(), key=lambda item: item[1],reverse=True):
+		l.append(key)
+		
+	print(l)
+	return l
+
+sorted_words("Book1.txt")
