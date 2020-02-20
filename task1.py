733ea1794ab5711431bc1d45c45791ac7388c966
Zhiming-Xiao <zh8935942dal.ca>
2020-02-20 10:16:30 -0800

    finish 2

--- task1.py
+++ task1.py
@@ -6,7 +6,22 @@ def sub_task1():
 	for i,r in df.iterrows():
 		d[r["CONDITION"]] = d.get(r["CONDITION"],[])
 	for i,r in df.iterrows():
-		d[r["CONDITION"]].append(r["OBJECTID"])
+		d[r["CONDITION"]].append(int(r["OBJECTID"]))
 	print(d)
 
-sub_task1()
+#sub_task1()
+
+def sub_task2():
+	df = pd.read_csv("Trails.csv",usecols=["OBJECTID","INST_YEAR"])
+	d = {}
+	for i,r in df.iterrows():
+		if r["INST_YEAR"] >= 2000:
+			iy = f"Installation Year for this {int(r['INST_YEAR'])}"
+			d[iy] = d.get(iy,[])
+	for i,r in df.iterrows():
+		if r["INST_YEAR"] >= 2000:
+			iy = f"Installation Year for this {int(r['INST_YEAR'])}"
+			d[iy].append(int(r["OBJECTID"]))
+	print(d)
+	
+sub_task2()
