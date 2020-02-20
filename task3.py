97a23de27dcc910e30f4844069a09e402a698715
Zhiming-Xiao <zh8935942dal.ca>
2020-02-20 11:32:38 -0800

    2

--- task3.py
+++ task3.py
@@ -10,4 +10,23 @@ def unique_words(b):
 	st = tuple(st)
 	return st
 	
-unique_words("Book1.txt")
+#unique_words("Book1.txt")
+
+def count_the_article(b):
+	with open(b,"r") as f:
+		ft = f.read()
+		
+	nt = ft.split()
+	
+	l = ["a", "the", "at", "run", "to","and","are","or","for","an","this"]
+	c = 0
+	for i in nt:
+		if i in l:
+			c += 1
+	print(c)
+	return c
+	
+count_the_article("Book1.txt")
+
+	
+	
